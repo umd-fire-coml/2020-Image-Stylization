facdb770235b4d014e0be18b23449e2a391f9eb1
Tianxiao Yang <tyang23@terpmail.umd.edu>
2020-10-21 15:11:27 -0400

    added dataset and dataloader test

---
+++ loader_test.py
@@ -0,0 +1,27 @@
+from Loader import CustomDataset
+import os
+import torch
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+my_dataset = CustomDataset(path='data')
+dataloader = DataLoader(my_dataset, batch_size=8,
+                        shuffle=True)
+
+#testing the dataset
+print("dataset test")
+#expected is first image dimension and name of the dataset
+print(my_dataset[0][0].shape)
+print(my_dataset[0][1])
+#testing the script
+print("dataloader test")
+for i_batch, batch in enumerate(dataloader):
+    #all of the batch[i] corresponds to what got returned from the loader    
+    #batch[0] is all of the np arrays
+    print("batch " + str(i_batch))
+    for x in batch[0]:
+        print(x.shape)
+    #batch[1] is all of the names
+    for x in batch[1]:
+        print(x)
